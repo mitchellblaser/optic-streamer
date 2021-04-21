c2c5f89dc4ca63371f7cb3a76e400828caf8694e
Mitch Blaser <mitchell.1080media@gmail.com>
2021-04-21 18:11:30 +1000

    Updated Client Script Python

--- opticpy.py
+++ opticpy.py
@@ -11,10 +11,16 @@ msg = """   ___       _   _    ___ _
        |_| github.com/mitchellblaser/optic-streamer 
        """
 
-addr = sys.argv[1]
-port = sys.argv[2]
-winScale = float(sys.argv[3])
-statInterval = int(sys.argv[4])
+if len(sys.argv) < 2:
+    addr = "10.10.10.182"
+    port = 8080
+    winScale = 1.5
+    statInterval = 10
+else:
+    addr = sys.argv[1]
+    port = sys.argv[2]
+    winScale = float(sys.argv[3])
+    statInterval = int(sys.argv[4])
 
 print(msg)
 print("Using Options: ADDR:" + addr + " PORT:" + str(port) + " SCALE:" + str(winScale) + " UPDATE:" + str(statInterval) + ".")
@@ -54,6 +60,8 @@ FPSCount = [0, 0, 0]
 Timer = 0
 TimerCache = 0
 
+print("FPS: 0", end='')
+
 while True:
     if(GetImage() == True):
         if time.time() > TimerCache+1:
@@ -64,7 +72,8 @@ while True:
             UpdateCounter = 0
             Timer = Timer + 1
             if Timer > statInterval:
-                print("FPS: " + str((FPSCount[0]+FPSCount[1]+FPSCount[2])/3))
+                print('\r', end='')
+                print("FPS: " + str((FPSCount[0]+FPSCount[1]+FPSCount[2])/3), end='')
                 Timer = 0
         cv2.imshow("OpticStreamer PyClient", img)
         if cv2.waitKey(1) == 27:
